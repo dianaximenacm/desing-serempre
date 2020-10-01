311b0376ae1f3e0ab3bae2d3cc23389fb9348a69
dianaximenacm <dianaximenacm@gmail.com>
2020-10-01 11:38:17 -0500

    [change] Port for Heroku Signed-off-by: dianaximenacm <dianaximenacm@gmail.com>

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get('/*', function(req, res) {
 });
 
 
-app.listen(process.env.PORT || 3000, () =>{
+app.listen(process.env.PORT || 8080, () =>{
     console.log('I´m working right! Yeah')
 });
