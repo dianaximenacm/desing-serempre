11785fadb37ab986fa92de51be413b6e2486229f
dianaximenacm <dianaximenacm@gmail.com>
2020-10-01 17:23:50 -0500

    [new] components and design header Signed-off-by: dianaximenacm <dianaximenacm@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { HeaderComponent } from './components/header/header.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductFooterComponent } from './components/product-footer/product-footer.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const routes: Routes = [
+  {path: 'header', component : HeaderComponent},
+  {path: 'product', component : ProductComponent},
+  {path: 'product-footer', component :  ProductFooterComponent},
+  {path: 'footer', component : FooterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductFooterComponent } from './components/product-footer/product-footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductComponent,
+    ProductFooterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
